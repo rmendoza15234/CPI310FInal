5dd1422e55cf43e1d16a0b97f9182f497167454f
rmendoza15234 <72532374+rmendoza15234@users.noreply.github.com>
2020-12-02 23:41:48 -0700

    game.js created

---
+++ game.js
@@ -0,0 +1,165 @@
+
+                //**************  DECLARING VARIABLES
+                //the player object
+                var flappyBird;
+
+                var flappyBeak;
+                //array of all the pipes to be generated
+                var pipes = [];
+                //score to keep track of points
+                
+                //*************************************
+                
+                //this function will load all the components when the page loads
+                function startGame() 
+                {
+                    //creating the flappy bird player
+                    flappyBird = new component(30, 30, "blue", 10, 120);
+                    flappyBeak = new component(20, 15, "orange", 40, 135);
+
+                    //gravity pulling down
+                    flappyBird.gravity = 0.05;
+                    flappyBeak.gravity = 0.05;
+                    //creating the score component
+                    export const score = new component("32px", "Times", "#eceddd", 280, 40, "text");
+                    //this will start the game
+                    myGameArea.start();
+                }
+                //************** CREATING THE GAME AREA
+                var myGameArea = 
+                {
+                    //canvas is where the game is drawn
+                    canvas : document.createElement("canvas"),
+                    start : function() 
+                    {
+                        //width of the canvas
+                        this.canvas.width = 720;
+                        //height of the canvas
+                        this.canvas.height = 450;
+
+                        //this defines the game as 2d
+                        this.context = this.canvas.getContext("2d");
+                        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+                        // this.canvas.fillStyle = "red";
+                        // this.canvas.fillRect(10,10,720,450);
+                        //start at the first frame
+                        this.frameNo = 0;
+                        //internal is updated
+                        this.interval = setInterval(updateGameArea, 20);
+                        },
+                    clear : function() 
+                    {
+                        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                    }
+                }
+                //*****************************************
+
+                //******** FOR DEBUGGING PURPOSES
+                let isStarted = 0;
+                //making sure the game starts only once
+                if(isStarted == 0)
+                {
+                    startGame();
+                    //will only run once until reloaded
+                    isStarted = 1;
+                }
+                //******************************
+            
+                
+                //****************** BASICALLY A CONSTRUCTOR SET UP FOR CREATING GAME COMPONENTS FOR PLAYER, SCORE, ETC.
+                function component(width, height, color, x, y, type) 
+                {
+                    this.type = type;
+                    this.score = 0;
+                    this.width = width;
+                    this.height = height;
+                    this.speedX = 0;
+                    this.speedY = 0;    
+                    this.x = x;
+                    this.y = y;
+                    this.gravity = 0;
+                    this.gravitySpeed = 0;
+                    this.update = function() {
+                        ctx = myGameArea.context;
+                        if (this.type == "text") {
+                            ctx.font = this.width + " " + this.height;
+                            ctx.fillStyle = color;
+                            ctx.fillText(this.text, this.x, this.y);
+                        } else {
+                            ctx.fillStyle = color;
+                            ctx.fillRect(this.x, this.y, this.width, this.height);
+                        }
+                    }
+                    this.newPos = function() {
+                        this.gravitySpeed += this.gravity;
+                        this.x += this.speedX;
+                        this.y += this.speedY + this.gravitySpeed;
+                        this.hitBottom();
+                    }
+                    this.hitBottom = function() {
+                        var rockbottom = myGameArea.canvas.height - this.height;
+                        if (this.y > rockbottom) {
+                            this.y = rockbottom;
+                            this.gravitySpeed = 0;
+                        }
+                    }
+                    this.crashWith = function(otherobj) {
+                        var myleft = this.x;
+                        var myright = this.x + (this.width);
+                        var mytop = this.y;
+                        var mybottom = this.y + (this.height);
+                        var otherleft = otherobj.x;
+                        var otherright = otherobj.x + (otherobj.width);
+                        var othertop = otherobj.y;
+                        var otherbottom = otherobj.y + (otherobj.height);
+                        var crash = true;
+                        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+                            crash = false;
+                        }
+                        return crash;
+                    }
+                }
+                //**************************************************************************
+                
+                function updateGameArea() {
+                    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+                    for (i = 0; i < pipes.length; i += 1) {
+                        if (flappyBird.crashWith(pipes[i])) 
+                        {
+                            return;
+                        } 
+                    }
+                    myGameArea.clear();
+                    myGameArea.frameNo += 1;
+                    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+                        x = myGameArea.canvas.width;
+                        minHeight = 20;
+                        maxHeight = 200;
+                        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+                        minGap = 50;
+                        maxGap = 200;
+                        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+                        pipes.push(new component(40, height, "green", x, 0));
+                        pipes.push(new component(40, x - height - gap, "green", x, height + gap));
+                    }
+                    for (i = 0; i < pipes.length; i += 1) {
+                        pipes[i].x += -1;
+                        pipes[i].update();
+                    }
+                    score.text="SCORE: " + myGameArea.frameNo;
+                    score.update();
+                    flappyBird.newPos();
+                    flappyBeak.newPos();
+                    flappyBird.update();
+                    flappyBeak.update();
+                }
+                
+                function everyinterval(n) {
+                    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+                    return false;
+                }
+                
+                function accelerate(n) {
+                    flappyBird.gravity = n;
+                    flappyBeak.gravity = n;
+                }
