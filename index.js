b32e1fca3417a0256a29716e0215dce4bed198ba
Alexander Martinez <59891955+A-man624@users.noreply.github.com>
2020-12-03 20:39:09 -0700

    got rid of the password getting displayed

--- index.js
+++ index.js
@@ -99,12 +99,11 @@ app.get("/profile", async (req, res) => {
         Scores.id,
         Scores.score,
         Users.username as playerName,
-        Users.email as playerEmail,
-        Users.password as playerPassword
+        Users.email as playerEmail
         FROM Scores LEFT JOIN Users WHERE Scores.playerId = Users.id;`); //grabs all of the scores along with the user
     console.log("profile", scores);
 
-    res.render("profile", { scores: scores, user: req.user, email: req.user.email, password: req.user.password }); //renders the standings page
+    res.render("profile", { scores: scores, user: req.user, email: req.user.email }); //renders the standings page
 });
 
 app.get("/forum", async (req, res) => {
